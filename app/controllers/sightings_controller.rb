4810efeb2399b9e4db2377a5ca250342b2c7ad01
Rique <aguirree1985@gmail.com>
2023-11-30 11:55:29 -0800

    Adds CRUD actions for sightings

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,38 @@
+class SightingsController < ApplicationController
+    def index
+        sighting = Sighting.all
+        render json: sighting
+    end
+    def show
+       sighting = Sighting.find(params[:id])
+       render json: sighting
+    end
+    def create
+        sighting = Sighting.create(sighting_params)
+        if sighting.valid?
+            render json: sighting
+        else render json: sighting.errors
+        end
+    end
+    def update
+        sighting = Sighting.find(params[:id])
+        sighting.update(sighting_params)
+        if sighting.valid?
+            render json: sighting
+        else
+            render sighting.errors
+        end
+    end
+    def destroy
+        sighting = Sighting.find(params[:id])
+        if sighting.destroy
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+    end
+    private
+    def sighting_params
+        params.require(:sighting).permit(:animal_id, :latitude, :longitude, :date)
+    end
+end
